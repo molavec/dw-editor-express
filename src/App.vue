0c51c69d9dd830006f824691322e8e3ae4750e00
Miguel Olave <molavec@gmail.com>
2023-03-24 18:37:08 -0300

    PropsTest para realizar una prueba de funcionamiento de los props

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <top-bar-app />
+  <div>
+    <p>
+      <button @click="addHandler">ADD ++</button>
+    </p>
+    <p>
+      <PropsTest :valor="valor"></PropsTest>
+    </p>
+
+  </div>
+
   <router-view></router-view>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import TopBarApp from './components/combined/TopBarApp.vue';
+import PropsTest from './components/commons/PropsTest.vue';
+
+const valor = ref(0);
+
+const addHandler = () => {
+  valor.value++;
+};
 </script>
 
 <style scoped></style> 
