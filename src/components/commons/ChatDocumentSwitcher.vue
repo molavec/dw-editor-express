43abf2fb3e3f2cd3bb29fb7b5d3dff5635ba70c2
Miguel Olave <molavec@gmail.com>
2023-04-12 22:49:40 -0400

    ajusta icono

--- src/components/commons/ChatDocumentSwitcher.vue
+++ src/components/commons/ChatDocumentSwitcher.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="flex items-center" @click="toggleIcon">
-    <svg v-if="isChatIcon" class="w-6 h-6 text-rose-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M20.314 3.686c-3.905-3.905-10.237-3.905-14.143 0-3.905 3.905-3.905 10.237 0 14.143m3.536-9.899v3h-3.536l-4 4v-4h-3.536v-3.536h3.536v-4l4 4z"/>
-    </svg>
-    <svg v-else class="w-6 h-6 text-rose-900" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M13 2l7 7-7 7M5 9h9a3 3 0 013 3v8a3 3 0 01-3 3H5a3 3 0 01-3-3V12a3 3 0 013-3z"/>
-    </svg>
+    <ChatBubbleBottomCenterIcon class="h-6 w-6 text-rose-700" v-if="isChatType" />
+    <DocumentTextIcon class="h-6 w-6 text-rose-700" v-else />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { ChatBubbleBottomCenterIcon } from '@heroicons/vue/24/outline';
+import { DocumentTextIcon } from '@heroicons/vue/24/outline';
 
-const isChatIcon = ref(true);
+const isChatType = ref(true);
+
+const emit = defineEmits(['editorTypeChange']);
 
 const toggleIcon = () => {
-  isChatIcon.value = !isChatIcon.value;
+  isChatType.value = !isChatType.value;
+  emit('editorTypeChange', isChatType.value);
 };
 
 </script>
